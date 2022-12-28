3bad25f2b325e1f84c9b5b11edb349fa489ba123
Podter <48936015+Podter@users.noreply.github.com>
2022-12-28 22:15:34 +0700

    more features

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -5,9 +5,11 @@ import {
   Previous20Filled,
   Speaker220Filled,
   SpeakerMute20Filled,
+  SkipBack1020Filled,
+  SkipForward1020Filled,
 } from "@fluentui/react-icons";
 import { useCurrentSong } from "../contexts/CurrentSongContext";
-import { useEffect, useRef, useState } from "react";
+import { SyntheticEvent, useEffect, useRef, useState } from "react";
 import { usePocketBase } from "../contexts/PocketBaseContext";
 import { useSongList } from "../contexts/SongListContext";
 
@@ -22,6 +24,9 @@ export default function Player() {
   const [audioSrc, setAudioSrc] = useState("");
   const [volume, setVolume] = useState(100);
   const [muted, setMuted] = useState(false);
+  const [duration, setDuration] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [newCurrentTime, setNewCurrentTime] = useState(0);
 
   function playBtn() {
     if (!currentSong.song) currentSong.setSong(songList.list[0]);
@@ -51,6 +56,11 @@ export default function Player() {
     }
   }
 
+  function audioOnTimeUpdate(event: SyntheticEvent<HTMLAudioElement, Event>) {
+    setCurrentTime(event.currentTarget.currentTime);
+    setDuration(event.currentTarget.duration);
+  }
+
   useEffect(() => {
     if (playing) {
       audio.current?.play();
@@ -71,50 +81,79 @@ export default function Player() {
     (audio.current as HTMLAudioElement).volume = muted ? 0 : volume / 100;
   }, [volume, muted]);
 
+  useEffect(() => {
+    (audio.current as HTMLAudioElement).currentTime = newCurrentTime;
+  }, [newCurrentTime]);
+
   return (
-    <div className="navbar bg-base-300 fixed bottom-0 z-40">
-      <div className="navbar-start">
-        <a
-          className="btn btn-ghost normal-case text-xl"
-          href={`#${currentSong.song?.id}`}
-        >
-          {currentSong.song?.title || ""}
-        </a>
+    <>
+      <div className="navbar bg-base-300 fixed bottom-0 z-30 min-h-[5rem]">
+        <div className="navbar-start">
+          <a
+            className="btn btn-ghost normal-case text-xl"
+            href={`#${currentSong.song?.id}`}
+          >
+            {currentSong.song?.title || ""}
+          </a>
+        </div>
+        <div className="navbar-center">
+          <button
+            className="btn btn-ghost btn-circle"
+            onClick={() => setNewCurrentTime(currentTime - 10)}
+          >
+            <SkipBack1020Filled />
+          </button>
+          <button className="btn btn-ghost btn-circle" onClick={skipBack}>
+            <Previous20Filled />
+          </button>
+          <button className="btn btn-ghost btn-circle" onClick={playBtn}>
+            {playing ? <Pause20Filled /> : <Play20Filled />}
+          </button>
+          <button className="btn btn-ghost btn-circle" onClick={skipNext}>
+            <Next20Filled />
+          </button>
+          <button
+            className="btn btn-ghost btn-circle"
+            onClick={() => {
+              setNewCurrentTime(currentTime + 10);
+            }}
+          >
+            <SkipForward1020Filled />
+          </button>
+        </div>
+        <div className="navbar-end">
+          <input
+            type="range"
+            min="0"
+            max="100"
+            value={muted ? 0 : volume}
+            className="range range-primary range-xs w-32"
+            onChange={(e) => (muted ? {} : setVolume(+e.target.value))}
+          />
+          <button
+            className="btn btn-ghost btn-circle"
+            onClick={() => setMuted(!muted)}
+          >
+            {muted ? <SpeakerMute20Filled /> : <Speaker220Filled />}
+          </button>
+        </div>
       </div>
-      <div className="navbar-center">
-        <button className="btn btn-ghost btn-circle" onClick={skipBack}>
-          <Previous20Filled />
-        </button>
-        <button className="btn btn-ghost btn-circle" onClick={playBtn}>
-          {playing ? <Pause20Filled /> : <Play20Filled />}
-        </button>
-        <button className="btn btn-ghost btn-circle" onClick={skipNext}>
-          <Next20Filled />
-        </button>
-      </div>
-      <div className="navbar-end">
-        <input
-          type="range"
-          min="0"
-          max="100"
-          value={muted ? 0 : volume}
-          className="range range-primary range-xs w-32"
-          onChange={(e) => (muted ? {} : setVolume(+e.target.value))}
-        />
-        <button
-          className="btn btn-ghost btn-circle"
-          onClick={() => setMuted(!muted)}
-        >
-          {muted ? <SpeakerMute20Filled /> : <Speaker220Filled />}
-        </button>
-      </div>
+      <input
+        type="range"
+        min="0"
+        max={duration}
+        value={currentTime}
+        className="range range-primary range-2xs w-screen bottom-0 fixed z-40 rounded-none"
+      />
       <audio
         ref={audio}
         src={audioSrc}
         onLoadedData={() => setPlaying(true)}
         onPause={() => setPlaying(false)}
         onPlay={() => setPlaying(true)}
+        onTimeUpdate={audioOnTimeUpdate}
+        onEnded={skipNext}
       />
-    </div>
+    </>
   );
 }
