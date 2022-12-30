d7757f7e47ee08c9e8c2e736970fef014ad37eb5
Podter <48936015+Podter@users.noreply.github.com>
2022-12-30 23:51:52 +0700

    disabled right click menu

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,12 @@ import Layout from "./components/Layout";
 import Home from "./pages/Home";
 // import Settings from "./pages/Settings";
 import Search from "./pages/Search";
+import { E } from "@tauri-apps/api/shell-cbf4da8b";
 
 export default function App() {
+  // Disable right click menu
+  document.addEventListener("contextmenu", (e) => e.preventDefault());
+
   return (
     <BrowserRouter>
       <Routes>
