f8cc142f98d41a7cd50088f829bcf3a5fa42ef3c
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-06-24 18:25:54 +0700

    added single-instance

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,19 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+
+#[derive(Clone, serde::Serialize)]
+struct Payload {
+    args: Vec<String>,
+    cwd: String,
+}
+
 fn main() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
+            app.emit_all("single-instance", Payload { args: argv, cwd })
+                .unwrap();
+        }))
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
