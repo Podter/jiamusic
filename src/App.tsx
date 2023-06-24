1140269d95919fefcce79ba17335dc30f7124629
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-06-24 15:55:31 +0700

    button

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import { Button } from "./components/ui/button";
+
 export default function App() {
-  return <div className="text-3xl font-bold underline">App</div>;
+  return (
+    <div>
+      <Button>Button</Button>
+    </div>
+  );
 }
