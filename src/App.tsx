b5f229c602f7815b6f617971758fdf2ac7d536f5
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-06-24 19:08:50 +0700

    disable right click menu

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 
 import Home from "./pages/Home";
+import { useEffect } from "react";
 
 export default function App() {
+  useEffect(() => {
+    document.addEventListener("contextmenu", (e) => e.preventDefault());
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
