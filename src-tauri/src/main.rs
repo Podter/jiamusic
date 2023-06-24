7c5f670d5d28515eabd141f6c5f2ab1fa50f79cb
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-06-24 18:57:25 +0700

    added autostart

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use tauri::Manager;
+use tauri_plugin_autostart::MacosLauncher;
 
 #[cfg(not(target_os = "linux"))]
 use window_shadows::set_shadow;
@@ -17,6 +18,10 @@ fn main() {
             app.emit_all("single-instance", Payload { args: argv, cwd })
                 .unwrap();
         }))
+        .plugin(tauri_plugin_autostart::init(
+            MacosLauncher::LaunchAgent,
+            Some(vec![]),
+        ))
         .setup(|app| {
             let window = app.get_window("main").unwrap();
 
--- src/components/Titlebar/Menu.tsx
+++ src/components/Titlebar/Menu.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import {
   Menubar,
   MenubarContent,
@@ -5,9 +6,17 @@ import {
   MenubarMenu,
   MenubarSeparator,
   MenubarTrigger,
+  MenubarCheckboxItem,
 } from "../ui/Menubar";
+import { enable, isEnabled, disable } from "tauri-plugin-autostart-api";
 
 export default function Menu() {
+  const [autostart, setAutostart] = useState<boolean | undefined>(undefined);
+
+  useEffect(() => {
+    isEnabled().then((enabled) => setAutostart(enabled));
+  }, []);
+
   return (
     <Menubar className="h-8 border-none ml-2">
       <MenubarMenu>
@@ -25,6 +34,21 @@ export default function Menu() {
         </MenubarTrigger>
         <MenubarContent>
           <MenubarItem>About JIΛmusic</MenubarItem>
+          <MenubarSeparator />
+          <MenubarCheckboxItem
+            checked={autostart}
+            onClick={async () => {
+              if (autostart) {
+                await disable();
+                setAutostart(false);
+              } else if (!autostart) {
+                await enable();
+                setAutostart(true);
+              }
+            }}
+          >
+            Autostart
+          </MenubarCheckboxItem>
           <MenubarSeparator />
           <MenubarItem>Hide Window</MenubarItem>
           <MenubarItem>Quit</MenubarItem>
