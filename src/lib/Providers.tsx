54a77fd74703ccaf4f05fbfd8afa16618eae0f87
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-06-24 19:05:20 +0700

    providers

---
+++ src/lib/Providers.tsx
@@ -0,0 +1,5 @@
+import type { PropsWithChildren } from "react";
+
+export default function Providers({ children }: PropsWithChildren) {
+  return <>{children}</>;
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+import Providers from "./lib/Providers";
 
 import "./styles.css";
 import "@fontsource/inter/latin.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Providers>
+      <App />
+    </Providers>
   </React.StrictMode>
 );
