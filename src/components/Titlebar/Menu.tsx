5f94f635f1b6662c0e7f05635feab09d0d57174f
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-06-25 21:23:21 +0700

    working about dialog

--- src/components/Titlebar/Menu.tsx
+++ src/components/Titlebar/Menu.tsx
@@ -22,6 +22,8 @@ import { enable, isEnabled, disable } from "tauri-plugin-autostart-api";
 import { appWindow } from "@tauri-apps/api/window";
 import useStore from "../../hooks/useStore";
 import { open } from "@tauri-apps/api/shell";
+import icon from "../../assets/icon.png";
+import logo from "../../assets/logo.png";
 
 export default function Menu() {
   const [autostart, setAutostart] = useState<boolean | undefined>(undefined);
@@ -53,7 +55,7 @@ export default function Menu() {
           >
             <span className="font-bold">
               <img
-                src="/jiamusic.png"
+                src={icon}
                 alt="JIΛmusic"
                 className="mr-[0.375rem]"
                 width={20}
@@ -124,12 +126,30 @@ export default function Menu() {
           </MenubarContent>
         </MenubarMenu>
       </Menubar>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Are you sure absolutely sure?</DialogTitle>
-          <DialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
+      <DialogContent className="max-w-sm">
+        <DialogHeader className="items-center text-center">
+          <img
+            src={logo}
+            alt="JIΛmusic"
+            className="h-32 w-32 mb-2"
+            width={128}
+            height={128}
+          />
+          <DialogTitle>JIΛmusic</DialogTitle>
+          <DialogDescription>2.0.0</DialogDescription>
+          <DialogDescription className="text-foreground text-base">
+            Music streaming service app by JIΛFEI
+          </DialogDescription>
+          <DialogDescription>Copyright © 2022-2023 Podter</DialogDescription>
+          <DialogDescription asChild>
+            <button
+              className="hover:underline underline-offset-4"
+              onClick={async () =>
+                await open("https://github.com/Podter/jiamusic")
+              }
+            >
+              GitHub repository
+            </button>
           </DialogDescription>
         </DialogHeader>
       </DialogContent>
