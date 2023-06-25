957a56b1fe17b2eb76cad61e13151c9018f55848
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-06-25 21:31:08 +0700

    added react-query

--- src/lib/Providers.tsx
+++ src/lib/Providers.tsx
@@ -1,11 +1,16 @@
 import type { PropsWithChildren } from "react";
 import { PocketBaseProvider } from "../contexts/PocketBaseContext";
 import { StoreProvider } from "../contexts/StoreContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function Providers({ children }: PropsWithChildren) {
   return (
     <StoreProvider>
-      <PocketBaseProvider>{children}</PocketBaseProvider>
+      <QueryClientProvider client={queryClient}>
+        <PocketBaseProvider>{children}</PocketBaseProvider>
+      </QueryClientProvider>
     </StoreProvider>
   );
 }
