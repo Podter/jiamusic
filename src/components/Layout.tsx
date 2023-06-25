c22f585ba2cccf403f42f7dc129e68864b7640a1
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-06-25 22:37:59 +0700

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,9 +6,9 @@ export default function Layout() {
   return (
     <>
       <Titlebar />
-      <div className="flex flex-row mt-10">
+      <div className="flex flex-row mt-10 h-[calc(100vh-2.5rem)]">
         <Sidebar />
-        <main>
+        <main className="overflow-y-scroll w-full">
           <Outlet />
         </main>
       </div>
