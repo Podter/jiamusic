c80053b146839d163042c17678706af5335fbda5
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-06-25 21:44:15 +0700

    added SongsProvider

---
+++ src/contexts/SongsContext.tsx
@@ -0,0 +1,43 @@
+import { useContext, createContext, type PropsWithChildren } from "react";
+import type { Song } from "../types/song";
+import { usePocketBase } from "./PocketBaseContext";
+import { useQuery, type QueryStatus } from "@tanstack/react-query";
+
+const SongsContext = createContext<
+  | {
+      songs: Song[];
+      refetch: () => void;
+    }
+  | undefined
+>(undefined);
+
+export function useSongs() {
+  const context = useContext(SongsContext);
+  if (!context) {
+    throw new Error("useSongs must be used within a SongsProvider");
+  }
+  return context;
+}
+
+export function SongsProvider({ children }: PropsWithChildren) {
+  const pb = usePocketBase();
+
+  const { data, refetch } = useQuery({
+    queryKey: ["songs"],
+    async queryFn() {
+      const songs = await pb.collection("songs").getFullList<Song>();
+      return songs;
+    },
+  });
+
+  return (
+    <SongsContext.Provider
+      value={{
+        songs: data ?? [],
+        refetch,
+      }}
+    >
+      {children}
+    </SongsContext.Provider>
+  );
+}
--- src/lib/Providers.tsx
+++ src/lib/Providers.tsx
@@ -2,6 +2,7 @@ import type { PropsWithChildren } from "react";
 import { PocketBaseProvider } from "../contexts/PocketBaseContext";
 import { StoreProvider } from "../contexts/StoreContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SongsProvider } from "../contexts/SongsContext";
 
 const queryClient = new QueryClient();
 
@@ -9,7 +10,9 @@ export default function Providers({ children }: PropsWithChildren) {
   return (
     <StoreProvider>
       <QueryClientProvider client={queryClient}>
-        <PocketBaseProvider>{children}</PocketBaseProvider>
+        <PocketBaseProvider>
+          <SongsProvider>{children}</SongsProvider>
+        </PocketBaseProvider>
       </QueryClientProvider>
     </StoreProvider>
   );
