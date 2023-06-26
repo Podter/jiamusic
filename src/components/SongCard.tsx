bb9cbbae1ef125da237ba06d3d166ffabcb6d62f
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-06-26 21:39:52 +0700

    Update SongCard.tsx

--- src/components/SongCard.tsx
+++ src/components/SongCard.tsx
@@ -4,6 +4,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "./ui/Card";
 import { Button } from "./ui/Button";
 import { Pause24Filled, Play24Filled } from "@fluentui/react-icons";
 import { cn } from "../lib/utils";
+import { usePlayer } from "../contexts/PlayerContext";
 
 type SongCardProps = {
   song: Song;
@@ -11,13 +12,14 @@ type SongCardProps = {
 
 export default function SongCard({ song }: SongCardProps) {
   const pb = usePocketBase();
+  const { song: currentSong, setSong, playing, togglePlayPause } = usePlayer();
 
   return (
     <Card
       className="overflow-hidden relative w-48 h-72 group hover:cursor-pointer"
-      // onClick={() => {
-      //   setSong(song);
-      // }}
+      onClick={() => {
+        setSong(song);
+      }}
     >
       <div className="relative bg-transparent overflow-hidden aspect-square">
         <img
@@ -26,20 +28,19 @@ export default function SongCard({ song }: SongCardProps) {
           width={192}
           height={192}
           className={cn(
-            "group-hover:scale-110 transition-transform"
-            // song.id === currentSong?.id && playing && "scale-110"
+            "group-hover:scale-110 transition-transform",
+            song.id === currentSong?.id && playing && "scale-110"
           )}
         />
         <Button
           className={cn(
-            "opacity-0 absolute bottom-2 right-2 rounded-full h-10 px-2 transition-opacity"
-            // song.id === currentSong?.id && "opacity-100"
+            "opacity-0 absolute bottom-2 right-2 rounded-full h-10 px-2 transition-opacity",
+            song.id === currentSong?.id && "opacity-100"
           )}
           size="lg"
-          // onClick={togglePlayPause}
+          onClick={togglePlayPause}
         >
-          {/* {playing ? <Pause24Filled /> : <Play24Filled />} */}
-          <Play24Filled />
+          {playing ? <Pause24Filled /> : <Play24Filled />}
         </Button>
       </div>
       <CardHeader className="p-3 pb-0">
