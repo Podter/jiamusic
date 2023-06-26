8dc9a7a6f8e4943f0a54e319a53a51016bb37f1e
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-06-26 22:54:42 +0700

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ export default function Songs() {
   return status === "success" ? (
     <div className="flex flex-col">
       <h2 className="mx-4 my-6 text-lg font-semibold tracking-tight">Home</h2>
-      <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4 h-full w-full p-6 pt-0">
+      <div className="grid grid-cols-[repeat(auto-fill,minmax(12rem,1fr))] gap-4 h-full w-full p-6 pt-0">
         {songs.map((song) => (
           <div
             key={song.id}
