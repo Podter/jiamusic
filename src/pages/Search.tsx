b5ee9556d7ee59bd5cb137cebde8dce29eb2c9d7
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-06-27 23:04:27 +0700

    working search

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,15 +1,45 @@
 import SongList from "../components/SongList";
 import { useSongs } from "../contexts/SongsContext";
+import { Input } from "../components/ui/Input";
+import { useEffect, useState } from "react";
+import Fuse from "fuse.js";
+import type { Song } from "../types/song";
+import { TableCaption } from "../components/ui/Table";
 
 export default function Search() {
   const { songs } = useSongs();
+  const [search, setSearch] = useState("");
+
+  const fuse = new Fuse(songs, {
+    keys: ["title"],
+  });
+
+  const [results, setResults] = useState<Song[]>([]);
+
+  useEffect(() => {
+    const list = fuse.search(search);
+    const songs = list.map((item) => item.item);
+    setResults(songs);
+  }, [search]);
 
   return (
     <div className="flex w-full flex-col">
       <div className="p-4 flex flex-row items-center justify-between">
         <h2 className="text-lg font-semibold tracking-tight">Search</h2>
+        <Input
+          className="w-72"
+          placeholder="Search"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
       </div>
-      <SongList songs={songs} />
+      {results.length > 0 ? (
+        <SongList songs={results} />
+      ) : !search ? (
+        <SongList songs={songs} />
+      ) : (
+        <TableCaption>No results</TableCaption>
+      )}
     </div>
   );
 }
