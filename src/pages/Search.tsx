ac449400b35ac43b7dc50e3d5a762897046e32fc
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-06-27 22:54:21 +0700

    basic table

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,3 +1,15 @@
+import SongList from "../components/SongList";
+import { useSongs } from "../contexts/SongsContext";
+
 export default function Search() {
-  return <div>Search</div>;
+  const { songs } = useSongs();
+
+  return (
+    <div className="flex w-full flex-col">
+      <div className="p-4 flex flex-row items-center justify-between">
+        <h2 className="text-lg font-semibold tracking-tight">Search</h2>
+      </div>
+      <SongList songs={songs} />
+    </div>
+  );
 }
