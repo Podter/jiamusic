6b5bd69b5e5d49e08c8e82252381475c61497a45
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-07-01 16:33:08 +0700

    notify update on launch

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,43 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import { useEffect } from "react";
+import { useToast } from "./hooks/useToast";
+import { checkUpdate, installUpdate } from "@tauri-apps/api/updater";
+import { relaunch } from "@tauri-apps/api/process";
+import { ToastAction } from "./components/ui/Toast";
 
 import Home from "./pages/Home";
 import Search from "./pages/Search";
 
 export default function App() {
+  const { toast } = useToast();
+
   useEffect(() => {
+    // Disable right click
     document.addEventListener("contextmenu", (e) => e.preventDefault());
+
+    // Check for updates
+    checkUpdate()
+      .then(({ shouldUpdate, manifest }) => {
+        if (shouldUpdate) {
+          toast({
+            title: "Update available",
+            description: `Version ${manifest?.version} is available.`,
+            action: (
+              <ToastAction
+                altText="Install and restart"
+                onClick={async () => {
+                  await installUpdate();
+                  await relaunch();
+                }}
+              >
+                Install and restart
+              </ToastAction>
+            ),
+          });
+        }
+      })
+      .catch((e) => console.error(e));
   }, []);
 
   return (
