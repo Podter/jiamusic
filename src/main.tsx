1216a3fa55e8f5e4fcad8cef3e19845704f5d34b
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-07-01 16:15:40 +0700

    added toast component

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import Providers from "./lib/Providers";
+import { Toaster } from "./components/ui/Toaster";
 
 import "./styles.css";
 import "@fontsource/inter/latin.css";
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Providers>
       <App />
+      <Toaster />
     </Providers>
   </React.StrictMode>
 );
